0d69d990aacc8013a39ea12062285cf220c72727
HolaSoyYoss <168798478+HolaSoyYoss@users.noreply.github.com>
2024-05-09 23:06:09 -0600

    Interfaz User terminada

--- client/src/app/components/user/user-index/user-index.component.ts
+++ client/src/app/components/user/user-index/user-index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User.model';
 
 @Component({
   selector: 'app-user-index',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-index.component.css']
 })
 export class UserIndexComponent {
-  Users:any = [
+  Users:User[] = [
     {id:1, name: "Luis", lastname: "Castillo Vera", email: "soyjoto@gmail.com", createdAt: "23/04/18"},
     {id:2, name: "Christian", lastname: "Castro Rios", email: "vampiro.castro@gmail.com", createdAt: "23/04/18"},
     {id:3, name: "Yoss", lastname: "Bello Jimenez", email: "bellojimenez@gmail.com", createdAt: "23/04/18"},
