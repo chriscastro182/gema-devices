ef0c035d7b71ed0b44fb91cd8270189c5f77483d
Christian Castro Ríos <deck.chris.182@gmail.com>
2024-05-27 23:32:37 -0600

    Boton logout y Get usuarios

--- client/src/app/components/user/user-index/user-index.component.ts
+++ client/src/app/components/user/user-index/user-index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/User.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-index',
@@ -7,9 +9,24 @@ import { User } from 'src/app/models/User.model';
   styleUrls: ['./user-index.component.css']
 })
 export class UserIndexComponent {
-  Users:User[] = [
-    {id:1, name: "Luis", lastname: "Castillo Vera", email: "soyjoto@gmail.com", createdAt: "23/04/18"},
-    {id:2, name: "Christian", lastname: "Castro Rios", email: "vampiro.castro@gmail.com", createdAt: "23/04/18"},
-    {id:3, name: "Yoss", lastname: "Bello Jimenez", email: "bellojimenez@gmail.com", createdAt: "23/04/18"},
-  ];
+  Users:[User] | undefined 
+
+  constructor(private userService:UserService, private authService:AuthService){
+    /* this.usuario = {_id:1, name: "Luis", lastname: "Castillo Vera", email: "soyjoto@gmail.com", createdAt: "23/04/18", roles:[]};
+    this.Users = [ this.usuario]; */
+  }
+
+  async ngOnInit(){
+    await this.userService.getUsers().subscribe(
+      res => {
+        console.log(res)
+        this.Users = res;
+      },
+      err => {
+        console.log(err)
+        this.authService.getSessionBehavior(err.status);
+      }
+    );
+  }
+
 }
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/User.model';
+import { catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private serverURL = "localhost";
+  private URL = `http://${this.serverURL}:3000/api/users`;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(){
+
+    return this.http.get<[User]>(this.URL).pipe(
+      catchError((err)=> {
+        console.error(err);
+        throw err;
+      })
+    );
+    
+  }
+  
+
+}
