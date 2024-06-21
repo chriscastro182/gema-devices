ce2084718b06aaaf76108f7a8e2661694fe30022
Christian Castro Ríos <deck.chris.182@gmail.com>
2024-06-20 22:36:04 -0600

    Borrar usuario con sweet alert

--- client/src/app/components/user/user-index/user-index.component.ts
+++ client/src/app/components/user/user-index/user-index.component.ts
@@ -7,6 +7,7 @@ import { EditModalComponent } from '../edit-modal/edit-modal.component';
 import { DeleteModalComponent } from '../delete-modal/delete-modal.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-index',
@@ -26,6 +27,9 @@ export class UserIndexComponent {
   }
 
   async ngOnInit(){
+    await this.getAllUsers();
+  }
+  async getAllUsers(){
     await this.userService.getUsers().subscribe(
       res => {
         console.log(res)
@@ -37,6 +41,40 @@ export class UserIndexComponent {
       }
     );
   }
+  ansDeleteUser(userId:any)
+  {
+    Swal.fire({
+      title: '¿Realmente deseas elminar este usuario?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      customClass: {
+        confirmButton: 'btn btn-danger mx-3', // Clase personalizada para el botón de confirmación
+        cancelButton: 'btn btn-secondary' // Clase personalizada para el botón de cancelar, si es necesario
+      },
+      buttonsStyling: false
+    }).then((result) => {
+      if (result.isConfirmed) { 
+        this.deleteUserById(userId);
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info')
+      }
+    })
+  }
+  deleteUserById(userId:any) { 
+    this.userService.deleteUserById(userId).subscribe({
+      next: (response) => {
+        Swal.fire('¡Éxito!', 'Usuario eliminado correctamente', 'success');
+
+        // función Refresh
+        this.getAllUsers();
+      }, error: (e) => {
+        Swal.fire('¡Error!', `No se pudo eliminar el Usuario: ${e.error.message}`, 'error');
+
+      }
+    })
+  }
+  
   openInfoModal(_id : string | undefined){
     if (_id) {
       let usuarioSeleccionado = this.Users?.find(u=>u._id == _id);
