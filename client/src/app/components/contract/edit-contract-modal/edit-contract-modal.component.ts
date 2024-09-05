a0f22c57ab9446efdc8648fba097e7a724673f6c
Christian Castro Ríos <deck.chris.182@gmail.com>
2024-09-04 22:25:24 -0600

    Le faltaba el update al metodo de contracts

--- client/src/app/components/contract/edit-contract-modal/edit-contract-modal.component.ts
+++ client/src/app/components/contract/edit-contract-modal/edit-contract-modal.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Contract } from 'src/app/models/Contract.model';
 import { ContractService } from 'src/app/services/contract.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-contract-modal',
@@ -20,6 +21,16 @@ export class EditContractModalComponent {
 
   // aqui debe ir el metodo para hacer la peticion
   updateContract(){
-    console.log(this.contract)
+    
+    this.contractService.updateContract(this.contract).subscribe({
+      next: async (response) => {
+        Swal.fire('¡Éxito!', 'Contrato actualizado correctamente', 'success');
+
+        // función Refresh        
+        this.contractUpdated.emit();
+      }, error: (e) => {
+        Swal.fire('¡Error!', `No se pudo actualizar el Contrato: ${e.error.message}`, 'error');
+      }
+    })
   }
 }
